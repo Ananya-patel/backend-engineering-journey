5b624c7e333fa8b5bc447f6de4a37fd1e1796f4d
midas jpmc <ananyapatel0407@gmail.com>
2026-02-03 09:48:26 +0530

    Day 48: Spring Boot setup and first REST API

---
+++ 03-spring-core/day48-spring-boot/springbootapi/springbootapi/src/main/java/com/example/springbootapi/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.springbootapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot backend ";
+    }
+}
