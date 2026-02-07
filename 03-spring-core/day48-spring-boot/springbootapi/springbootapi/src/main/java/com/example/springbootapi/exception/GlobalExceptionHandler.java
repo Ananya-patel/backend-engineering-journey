e6458ba5a6d6ba9be16b8955429717f23fa6e6ae
midas jpmc <ananyapatel0407@gmail.com>
2026-02-07 08:00:22 +0530

    Day 66: Input validation with @Valid and global handling

--- 03-spring-core/day48-spring-boot/springbootapi/springbootapi/src/main/java/com/example/springbootapi/exception/GlobalExceptionHandler.java
+++ 03-spring-core/day48-spring-boot/springbootapi/springbootapi/src/main/java/com/example/springbootapi/exception/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.example.springbootapi.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import jakarta.servlet.http.HttpServletRequest;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -37,4 +42,19 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+public ResponseEntity<Map<String, String>> handleValidationErrors(
+        MethodArgumentNotValidException ex) {
+
+    Map<String, String> errors = new HashMap<>();
+
+    ex.getBindingResult()
+      .getFieldErrors()
+      .forEach(error ->
+          errors.put(error.getField(), error.getDefaultMessage())
+      );
+
+    return ResponseEntity.badRequest().body(errors);
+}
+
 }
